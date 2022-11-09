a7ebade95491a0f4f4d6c16d6cc0c65bf1cbe9bb
Akash Chanara <akashchanara@gmail.com>
2022-11-08 22:03:11 -0800

    seed comments

---
+++ seed/comment-seed.js
@@ -0,0 +1,24 @@
+const { Comments } = require("../models");
+const sequelize = require("../config/connection");
+
+const commentData = [
+  {
+    comment_text: "Good!",
+    user_id: 1,
+    post_id: 1,
+  },
+  {
+    comment_text: "best",
+    user_id: 2,
+    post_id: 2,
+  },
+  {
+    comment_text: "awesome",
+    user_id: 3,
+    post_id: 3,
+  },
+];
+
+const seedComment = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComment;
