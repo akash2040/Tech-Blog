dcd5600adc37d5a5f72756049cb72a6d2c12e52f
Akash Chanara <akashchanara@gmail.com>
2022-11-08 19:50:41 -0800

    server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const routes = require("./controllers");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const path = require("path");
+
+// helper function
+const helpers = require("./utils/helper");
+
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// set Handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+// connection db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
